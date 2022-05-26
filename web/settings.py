082334b9aab4488a9a9b10bbd4a90e08a5d42ab3
Yur4ik1234 <60612565+Yur4ik1234@users.noreply.github.com>
2022-05-26 22:03:35 +0300

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -127,16 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static-cdn')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'web/static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-# Media Files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Base url to serve media files
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Crispy Settings
 
